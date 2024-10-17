9575b464ef3699f34a492069421a54fc3473aac2
wjdtnd <gkssk15963@gmail.com>
2024-10-17 09:54:16 +0900

    241017 업캐스팅

--- src/ch10/ex10_4/Square.java
+++ src/ch10/ex10_4/Square.java
@@ -1,5 +1,11 @@
 package ch10.ex10_4;
 
 public class Square extends Shape{
+    int pointNum=4;
+
+    public double area(int width){
+        double area=Math.pow(width,2);
+        return area;
+    }
 
 }
